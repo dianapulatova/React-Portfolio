4d3b7ec222717777885c00811419c5f4ecaa82d5
dianapulatova <65940970+dianapulatova@users.noreply.github.com>
2020-09-02 03:25:11 +0000

    Deploying to gh-pages from  @ c7deb64eac93fe658b08adb10febb06ec05de039 🚀

---
+++ precache-manifest.253220314d05687180c0b051e5efeeb2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b60416459ee88964e3d2bcf28f407e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7d7c0b14c3eadfca3fa",
+    "url": "/static/css/3.c7b31c95.chunk.css"
+  },
+  {
+    "revision": "bd8645de1d58c9c577f9",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "837d0f0e16ba6c6b12a6",
+    "url": "/static/css/main.d24cb4d7.chunk.css"
+  },
+  {
+    "revision": "28eaa00e78a52c8c6f2b",
+    "url": "/static/js/2.594460b7.chunk.js"
+  },
+  {
+    "revision": "d7d7c0b14c3eadfca3fa",
+    "url": "/static/js/3.05b3842d.chunk.js"
+  },
+  {
+    "revision": "bd8645de1d58c9c577f9",
+    "url": "/static/js/4.812e1d22.chunk.js"
+  },
+  {
+    "revision": "837d0f0e16ba6c6b12a6",
+    "url": "/static/js/main.d0f91c64.chunk.js"
+  },
+  {
+    "revision": "b762a221004e7342f712",
+    "url": "/static/js/runtime-main.0d25b0af.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "2da7901ea73c6aaf906528c317ba7615",
+    "url": "/static/media/contactMail.2da7901e.svg"
+  },
+  {
+    "revision": "7316a87c94c72171d4c577318afc7829",
+    "url": "/static/media/developerActivity.7316a87c.svg"
+  },
+  {
+    "revision": "8f3fa4ae5d582e200332f9eb67a13042",
+    "url": "/static/media/freelancer-1.8f3fa4ae.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "e27af204934f82059e8cd90fc66d2f4e",
+    "url": "/static/media/skill.e27af204.svg"
+  }
+]);
